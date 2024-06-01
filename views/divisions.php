1af8bdec581fefbc3cecfb98485fa97f82a421ff
Sahidur Rahman <me.sahid@gmail.com>
2024-06-01 18:50:33 +0600

    fix division

--- views/divisions.php
+++ views/divisions.php
@@ -5,5 +5,6 @@ include '../includes/db.php';
 $stmt = $db->prepare("SELECT * FROM divisions");
 $stmt->execute();
 $divisions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+header('Content-Type: application/json');
 echo json_encode($divisions);
 ?>
