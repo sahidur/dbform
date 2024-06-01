c4bb2608b3bdbb4baf2305fd3b9dcdd9c168e9e6
Sahidur Rahman <me.sahid@gmail.com>
2024-06-01 23:56:23 +0600

    view data

--- views/view_data2.php
+++ views/view_data2.php
@@ -1,20 +1,3 @@
-<?php
-// views/view_data.php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    header('Location: signin.php');
-    exit();
-}
-
-include '../includes/db.php';
-
-$user_id = $_SESSION['user_id'];
-
-$stmt = $db->prepare("SELECT * FROM user_data WHERE user_id = :user_id");
-$stmt->bindParam(':user_id', $user_id);
-$stmt->execute();
-$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -91,54 +74,7 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <div id="canvas">
             <!-- Dynamic content will be loaded here -->
             <h1>Dashboard</h1>
-            <table id="myDataTable" class="display" style="width:100%">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>User ID</th>
-            <th>Full Name</th>
-            <th>Division</th>
-            <th>District</th>
-            <th>Upazilla</th>
-            <th>Age</th>
-            <th>Salary</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-            foreach ($data as $row) {
-                echo '<tr>';
-                echo '<td>' . $row['id'] . '</td>';
-                echo '<td>' . $row['user_id'] . '</td>';
-                echo '<td>' . $row['full_name'] . '</td>';
-                echo '<td>' . $row['division'] . '</td>';
-                echo '<td>' . $row['district'] . '</td>';
-                echo '<td>' . $row['upazilla'] . '</td>';
-                echo '<td>' . $row['age'] . '</td>';
-                echo '<td>' . $row['salary'] . '</td>';
-                echo '</tr>';
-            }
-        ?>
-    </tbody>
-</table>
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.flash.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('#myDataTable').DataTable({
-            dom: 'Bfrtip',
-            buttons: [
-                'copy', 'csv', 'excel', 'pdf', 'print'
-            ]
-        });
-    });
-</script>
+            <?php include 'view_data.php'; ?>
         </div>
     </div>
 
