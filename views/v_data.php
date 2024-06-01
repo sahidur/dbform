addfede3c43d09256df9958a5d001dcf85b647d5
Sahidur Rahman <me.sahid@gmail.com>
2024-06-01 23:11:31 +0600

    fix data tables

--- views/v_data.php
+++ views/v_data.php
@@ -13,7 +13,7 @@ $user_id = $_SESSION['user_id'];
 $stmt = $db->prepare("SELECT * FROM user_data WHERE user_id = :user_id");
 $stmt->bindParam(':user_id', $user_id);
 $stmt->execute();
-$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -58,12 +58,19 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.flash.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
 <script>
     $(document).ready(function() {
-        $('#myDataTable').DataTable();
-        buttons: [
+        $('#myDataTable').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
                 'copy', 'csv', 'excel', 'pdf', 'print'
             ]
+        });
     });
 </script>
 
