2507b4c9d3ab28f611928961e1aedbf444d39376
Sahidur Rahman <me.sahid@gmail.com>
2024-06-01 21:12:11 +0600

    db update

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,6 @@
 <?php
 // includes/db.php
-$host = "db-postgresql-somadhan-do-user-2226216-0.c.db.ondigitalocean.com";
-$dbname = "kfw-data";
-$user = "kfwadmin"; // Change to your PostgreSQL username
-$password = "AVNS_Pu-s3I8f"; // Change to your PostgreSQL password
-$port = 25060;
+
 
 
 
