f021bf7bd194eef00ce62cd96ea32fc2c0257b23
Sahidur Rahman <me.sahid@gmail.com>
2024-06-01 20:56:30 +0600

    fix

--- views/add_data.php
+++ views/add_data.php
@@ -31,9 +31,9 @@ $upazilla = $stmt->fetchColumn();
 $stmt = $db->prepare("INSERT INTO data (user_id, full_name, division, district, upazilla, age, salary) VALUES (:user_id, :full_name, :division, :district, :upazilla, :age, :salary)");
 $stmt->bindParam(':user_id', $user_id);
 $stmt->bindParam(':full_name', $full_name);
-$stmt->bindParam(':division', $division);
-$stmt->bindParam(':district', $district);
-$stmt->bindParam(':upazilla', $upazilla);
+//$stmt->bindParam(':division', $division);
+//$stmt->bindParam(':district', $district);
+//$stmt->bindParam(':upazilla', $upazilla);
 $stmt->bindParam(':age', $age);
 $stmt->bindParam(':salary', $salary);
 
