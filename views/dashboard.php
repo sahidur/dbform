6daa2ffac700b43a7b36df70fc311b4564278522
Sahidur Rahman <me.sahid@gmail.com>
2024-06-02 09:48:00 +0600

    update form data all required

--- views/dashboard.php
+++ views/dashboard.php
@@ -37,25 +37,25 @@
             <!-- Beneficiary ID -->
             <div class="form-group">
                 <label for="beneficiaryId">Beneficiary ID (Start with KFW4-CH-/KFW4-BISD-)</label>
-                <input type="text" class="form-control" id="beneficiaryId" placeholder="Enter Beneficiary ID">
+                <input type="text" class="form-control" id="beneficiaryId" placeholder="Enter Beneficiary ID" required>
             </div>
 
             <!-- Name of beneficiary/ HH Head -->
             <div class="form-group">
                 <label for="beneficiaryName">Name of Beneficiary/ HH Head</label>
-                <input type="text" class="form-control" id="beneficiaryName" placeholder="Enter Name">
+                <input type="text" class="form-control" id="beneficiaryName" placeholder="Enter Name" required>
             </div>
 
             <!-- Guardian (Fathers/Mothers/Husband) name -->
             <div class="form-group">
                 <label for="guardianName">Guardian (Fathers/Mothers/Husband) Name</label>
-                <input type="text" class="form-control" id="guardianName" placeholder="Enter Guardian's Name">
+                <input type="text" class="form-control" id="guardianName" placeholder="Enter Guardian's Name" required>
             </div>
 
             <!-- Sex -->
             <div class="form-group">
                 <label for="sex">Sex</label>
-                <select class="form-control" id="sex">
+                <select class="form-control" id="sex" required>
                     <option>Male</option>
                     <option>Female</option>
                     <option>Third Gender</option>
@@ -66,19 +66,19 @@
             <!-- Age -->
             <div class="form-group">
                 <label for="age">Age</label>
-                <input type="number" class="form-control" id="age" placeholder="Enter Age">
+                <input type="number" class="form-control" id="age" placeholder="Enter Age" required>
             </div>
 
             <!-- National ID/Birth Registration -->
             <div class="form-group">
                 <label for="nationalId">National ID/Birth Registration</label>
-                <input type="text" class="form-control" id="nationalId" placeholder="Enter National ID or Birth Registration">
+                <input type="text" class="form-control" id="nationalId" placeholder="Enter National ID or Birth Registration" required>
             </div>
 
             <!-- Mobile Number -->
             <div class="form-group">
                 <label for="mobileNumber">Mobile Number</label>
-                <input type="text" class="form-control" id="mobileNumber" placeholder="Enter Mobile Number">
+                <input type="text" class="form-control" id="mobileNumber" placeholder="Enter Mobile Number" required>
             </div>
 
             <!-- Current Division -->
@@ -98,25 +98,25 @@
             <!-- Current City Corporation/Municipality/ Union -->
             <div class="form-group">
                 <label for="currentCityCorporation">Current City Corporation/Municipality/ Union</label>
-                <input type="text" class="form-control" id="currentCityCorporation" placeholder="Enter City Corporation/Municipality/Union">
+                <input type="text" class="form-control" id="currentCityCorporation" placeholder="Enter City Corporation/Municipality/Union" required>
             </div>
 
             <!-- Current Ward No -->
             <div class="form-group">
                 <label for="currentWardNo">Current Ward No</label>
-                <input type="text" class="form-control" id="currentWardNo" placeholder="Enter Ward No">
+                <input type="text" class="form-control" id="currentWardNo" placeholder="Enter Ward No" required>
             </div>
 
             <!-- Current Slum Name/Village /Area Name -->
             <div class="form-group">
                 <label for="currentSlumName">Current Slum Name/Village/Area Name</label>
-                <input type="text" class="form-control" id="currentSlumName" placeholder="Enter Slum/Village/Area Name">
+                <input type="text" class="form-control" id="currentSlumName" placeholder="Enter Slum/Village/Area Name" required>
             </div>
 
             <!-- Is the HH female headed -->
             <div class="form-group">
                 <label for="isFemaleHeaded">Is the HH Female Headed</label>
-                <select class="form-control" id="isFemaleHeaded">
+                <select class="form-control" id="isFemaleHeaded" required>
                     <option>Yes</option>
                     <option>No</option>
                 </select>
@@ -125,13 +125,13 @@
             <!-- HH average income (monthly) -->
             <div class="form-group">
                 <label for="hhIncome">HH Average Income (Monthly)</label>
-                <input type="number" class="form-control" id="hhIncome" placeholder="Enter Average Income">
+                <input type="number" class="form-control" id="hhIncome" placeholder="Enter Average Income" required>
             </div>
 
             <!-- Climate Migrant/displaced -->
             <div class="form-group">
                 <label for="climateMigrant">Climate Migrant/Displaced?</label>
-                <select class="form-control" id="climateMigrant">
+                <select class="form-control" id="climateMigrant" required>
                     <option>Yes</option>
                     <option>No</option>
                 </select>
@@ -140,33 +140,33 @@
             <!-- Division -->
             <div class="form-group">
                                 <label for="pdivision">Permanent Division</label>
-                                <select name="pdivision" id="pdivision" class="form-control" required></select>
+                                <select name="pdivision" id="pdivision" class="form-control" required></select required>
                             </div>
                             <div class="form-group">
                                 <label for="pdistrict">Permanent District</label>
-                                <select name="pdistrict" id="pdistrict" class="form-control" required></select>
+                                <select name="pdistrict" id="pdistrict" class="form-control" required></select required>
                             </div>
                             <div class="form-group">
                                 <label for="pupazilla">Permanent Upazila/Thana</label>
-                                <select name="pupazilla" id="pupazilla" class="form-control" required></select>
+                                <select name="pupazilla" id="pupazilla" class="form-control" required></select required>
                             </div>
 
             <!-- Slum Name/Village /Area Name -->
             <div class="form-group">
                 <label for="slumName">Slum Name/Village/Area Name</label>
-                <input type="text" class="form-control" id="slumName" placeholder="Enter Slum/Village/Area Name">
+                <input type="text" class="form-control" id="slumName" placeholder="Enter Slum/Village/Area Name" required>
             </div>
 
             <!-- Cause of climate migration -->
             <div class="form-group">
                 <label for="climateMigrationCause">Cause of Climate Migration</label>
-                <input type="text" class="form-control" id="climateMigrationCause" placeholder="Enter Cause of Climate Migration">
+                <input type="text" class="form-control" id="climateMigrationCause" placeholder="Enter Cause of Climate Migration" required>
             </div>
 
-            <!-- Disability (PWD) Status -->
+            <!-- Disability Status -->
             <div class="form-group">
-                <label for="disabilityStatus">Disability (PWD) Status</label>
-                <select class="form-control" id="disabilityStatus">
+                <label for="disabilityStatus">Students with disabilities Status</label>
+                <select class="form-control" id="disabilityStatus" required>
                     <option>Yes</option>
                     <option>No</option>
                 </select>
@@ -175,13 +175,13 @@
             <!-- Number of disabled member in the family (if any) -->
             <div class="form-group">
                 <label for="disabledMembers">Number of Disabled Members in the Family (if any)</label>
-                <input type="number" class="form-control" id="disabledMembers" placeholder="Enter Number of Disabled Members">
+                <input type="number" class="form-control" id="disabledMembers" placeholder="Enter Number of Disabled Members" required>
             </div>
 
             <!-- Is the beneficiary belong to any ethnic group -->
             <div class="form-group">
                 <label for="ethnicGroup">Is the Beneficiary Belong to Any Ethnic Group</label>
-                <select class="form-control" id="ethnicGroup">
+                <select class="form-control" id="ethnicGroup" required>
                     <option>Yes</option>
                     <option>No</option>
                 </select>
@@ -190,69 +190,67 @@
             <!-- Male household members -->
             <div class="form-group">
                 <label for="maleMembers">Male Household Members</label>
-                <input type="number" class="form-control" id="maleMembers" placeholder="Enter Number of Male Household Members">
+                <input type="number" class="form-control" id="maleMembers" placeholder="Enter Number of Male Household Members" required>
             </div>
 
             <!-- Female household members -->
             <div class="form-group">
                 <label for="femaleMembers">Female Household Members</label>
-                <input type="number" class="form-control" id="femaleMembers" placeholder="Enter Number of Female Household Members">
+                <input type="number" class="form-control" id="femaleMembers" placeholder="Enter Number of Female Household Members" required>
             </div>
 
             <!-- Third gender household members -->
             <div class="form-group">
-                <label for="thirdGenderMembers">Third Gender Household Members</
-
-label>
-                <input type="number" class="form-control" id="thirdGenderMembers" placeholder="Enter Number of Third Gender Household Members">
+                <label for="thirdGenderMembers">Third Gender Household Members</label>
+                <input type="number" class="form-control" id="thirdGenderMembers" placeholder="Enter Number of Third Gender Household Members" required>
             </div>
 
             <!-- Boy (bellow 18 Year) -->
             <div class="form-group">
                 <label for="boysUnder18">Boys (below 18 Year)</label>
-                <input type="number" class="form-control" id="boysUnder18" placeholder="Enter Number of Boys">
+                <input type="number" class="form-control" id="boysUnder18" placeholder="Enter Number of Boys" required>
             </div>
 
             <!-- Girl (bellow 18 Year) -->
             <div class="form-group">
                 <label for="girlsUnder18">Girls (below 18 Year)</label>
-                <input type="number" class="form-control" id="girlsUnder18" placeholder="Enter Number of Girls">
+                <input type="number" class="form-control" id="girlsUnder18" placeholder="Enter Number of Girls" required>
             </div>
 
             <!-- Third Gender (bellow 18 Year) -->
             <div class="form-group">
                 <label for="thirdGenderUnder18">Third Gender (below 18 Year)</label>
-                <input type="number" class="form-control" id="thirdGenderUnder18" placeholder="Enter Number of Third Gender">
+                <input type="number" class="form-control" id="thirdGenderUnder18" placeholder="Enter Number of Third Gender" required>
             </div>
 
             <!-- Adolescents boy (13-19 years) -->
             <div class="form-group">
                 <label for="adolescentBoys">Adolescents Boys (13-19 years)</label>
-                <input type="number" class="form-control" id="adolescentBoys" placeholder="Enter Number of Adolescent Boys">
+                <input type="number" class="form-control" id="adolescentBoys" placeholder="Enter Number of Adolescent Boys" required>
             </div>
 
             <!-- Adolescents girl (13-19 years) -->
             <div class="form-group">
                 <label for="adolescentGirls">Adolescents Girls (13-19 years)</label>
-                <input type="number" class="form-control" id="adolescentGirls" placeholder="Enter Number of Adolescent Girls">
+                <input type="number" class="form-control" id="adolescentGirls" placeholder="Enter Number of Adolescent Girls" required>
             </div>
 
             <!-- Adolescents third gender (13-19 years) -->
             <div class="form-group">
                 <label for="adolescentThirdGender">Adolescents Third Gender (13-19 years)</label>
-                <input type="number" class="form-control" id="adolescentThirdGender" placeholder="Enter Number of Adolescent Third Gender">
+                <input type="number" class="form-control" id="adolescentThirdGender" placeholder="Enter Number of Adolescent Third Gender" required>
             </div>
 
             <!-- Name of training received -->
             <div class="form-group">
                 <label for="trainingName">Name of Training Received</label>
-                <input type="text" class="form-control" id="trainingName" placeholder="Enter Name of Training">
+                <input type="text" class="form-control" id="trainingName" placeholder="Enter Name of Training" required>
             </div>
 
             <!-- Remarks -->
             <div class="form-group">
                 <label for="remarks">Remarks</label>
-                <textarea class="form-control" id="remarks" rows="3" placeholder="Enter Remarks"></textarea>
+                <textarea class="form-control" id="remarks" rows="3" placeholder="Enter Remarks" required></textarea>
             </div>
 
             <button type="submit" class="btn btn-primary">Submit</button>
