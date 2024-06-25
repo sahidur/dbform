ad623ba48ae5416bb9d79ad23ea758216ab386d1
Sahidur Rahman <me.sahid@gmail.com>
2024-06-25 16:30:55 +0600

    username fix

--- views/view_data.php
+++ views/view_data.php
@@ -179,7 +179,7 @@ $dataUser = $stmt1->fetchAll(PDO::FETCH_ASSOC);
                         echo '<td>' . $row['batch_end_date'] . '</td>';
                         echo '<td>' . $row['remarks'] . '</td>';
                         echo '<td>' . $row['created_date'] . '</td>';
-                        echo '<td>' . print_r($dataUser); . '</td>';
+                        echo '<td>' . $dataUser['username'] . '</td>';
                         echo '</tr>';
                     }
                 ?>
